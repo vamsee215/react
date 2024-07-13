51d6d390686003b6970844cfc71495a3941ddf1f
vamsee215 <140395680+vamsee215@users.noreply.github.com>
2024-07-13 16:23:31 +0530

    ep

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+
+
+
+
+
+
+const jsxHeading = <h1 id = "heading">NAMASTE JSX</h1>
+
+console.log(jsxHeading);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(jsxHeading);
+
